d605e8770221dd2aa4a73139c0d498b32ebe09d6
Iddo Kenigsberg <Iddo.Kenigsberg@post.runi.ac.il>
2025-10-30 20:41:07 +0200

    third try

--- NumWords.java
+++ NumWords.java
@@ -2,9 +2,9 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int n = Integer.parseInt(args[0]);
-		int hundreds = n / 100 % 10;
+		int hundreds = n / 100 ;
 		int tens = n / 10 % 10;
 		int ahadot = n % 10;
-		System.out.println(hundreds +" Hundreds, " + tens + " tens," + ahadot + " ones");
+		System.out.println(hundreds +" Hundreds, " + tens + " tens, and " + ahadot + " ones");
 	}
 }
